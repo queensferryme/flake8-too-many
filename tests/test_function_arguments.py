c73ff0afdc22409ea9750e7f00156e0e0391c8f0
Queensferry <queensferry.me@gmail.com>
2021-08-13 20:34:58 +0800

    ci: add github test action

--- tests/test_function_arguments.py
+++ tests/test_function_arguments.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 from flake8_too_many.messages import TMN001
 
@@ -11,7 +12,10 @@ def test() -> None:
     process = subprocess.run(["flake8", file], stdout=subprocess.PIPE)
     e1, e2, e3, count = process.stdout.decode("utf-8").splitlines()
     assert e1.endswith(f"2:1: {TMN001.format(6, 5)}")
-    assert e2.endswith(f"7:7: {TMN001.format(7, 5)}")
+    if sys.version_info < (3, 7):
+        assert e2.endswith(f"7:7: {TMN001.format(7, 5)}")
+    else:
+        assert e2.endswith(f"7:1: {TMN001.format(7, 5)}")
     assert e3.endswith(f"12:1: {TMN001.format(7, 5)}")
     assert int(count) == 3
 
@@ -22,6 +26,9 @@ def test_with_args() -> None:
         ["flake8", "--max-function-arguments=6", file], stdout=subprocess.PIPE
     )
     e1, e2, count = process.stdout.decode("utf-8").splitlines()
-    assert e1.endswith(f"7:7: {TMN001.format(7, 6)}")
+    if sys.version_info < (3, 7):
+        assert e1.endswith(f"7:7: {TMN001.format(7, 6)}")
+    else:
+        assert e1.endswith(f"7:1: {TMN001.format(7, 6)}")
     assert e2.endswith(f"12:1: {TMN001.format(7, 6)}")
     assert int(count) == 2
